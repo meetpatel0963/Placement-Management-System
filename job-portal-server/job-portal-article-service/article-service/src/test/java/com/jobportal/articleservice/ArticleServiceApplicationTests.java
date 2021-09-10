bf205d5b4479363278ef0485c9753630906682ef
meetpatel0963 <meetpatel0963@gmail.com>
2021-09-10 14:45:23 +0530

    minor changes in article-service

---
+++ job-portal-server/job-portal-article-service/article-service/src/test/java/com/jobportal/articleservice/ArticleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobportal.articleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
