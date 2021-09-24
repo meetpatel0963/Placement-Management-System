222ac91a7b2a47a649c6879d9ebb3f5057b5bd3a
meetpatel0963 <meetpatel0963@gmail.com>
2021-09-24 15:03:04 +0530

    removed article-service (spring boot), added article-service (python)

--- job-portal-server/job-portal-article-service/article-service/src/main/java/com/jobportal/articleservice/repository/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jobportal.articleservice.repository;
-
-import com.jobportal.articleservice.model.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
