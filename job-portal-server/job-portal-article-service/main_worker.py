f19f59307f4f2f98ce02f01d70e9216990a34670
Poojan Patel <poojan4004@gmail.com>
2021-09-29 19:42:39 +0530

    Centralized Config Added using RabbitMQ

--- job-portal-server/job-portal-article-service/main_worker.py
+++ job-portal-server/job-portal-article-service/main_worker.py
@@ -4,32 +4,57 @@ from subprocess import Popen, call
 import time
 import signal
 import os
+import pika, sys, os
+import json
 
 commands = [
      "uvicorn rest-server:app --port {}".format(settings.get_fresh('REST_PORT')),
      #'uvicorn grpc-server:serve --port 8000 --app-dir ./grpc_service'
      'py grpc_service/grpc-server.py',
-     #'py naming_server.py'
+     'py naming_server.py'
 ]
 
+def updateConfigEnv():
+     r=requests.get(settings.get_fresh('CONFIG_SERVER'))
+     resp = r.json()
+     if(resp.get('status',0) == 404):
+          print('Unable to connect')
+          return
+     else:
+          resp = resp['propertySources'][0]['source']
+          json.dump(resp,open("./env.json","w"))
+          broker()
+          return
 
 def startListening():
      process = [Popen(cmd) for cmd in commands]
 
-     # time.sleep(14)
+     time.sleep(14)
      # for i in range(len(process)):
      #      process[i].terminate()
      #      time.sleep(15)
      #      process[i] = Popen(commands[i])
-     # r=requests.get(settings.get_fresh('CONFIG_SERVER'))
-     # resp = r.json()
-     # if(resp.get('status',0) == 404):
-     #      print('Unable to connect')
-     #      return
-     # else:
-     #      resp = resp['propertySources'][0]['source']
-     #      json.dump(resp,open("./env.json","w"))
-         
-     #      return
+     updateConfigEnv()
+
+#startListening()
+def broker():
+     connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.get_fresh('AMQP_HOST')))
+     channel = connection.channel()
+     
+     result = channel.queue_declare(queue=settings.get_fresh('APP_NAME'), exclusive=True)
+     queue_name = result.method.queue
+
+     channel.queue_bind(exchange='springCloudBus', queue=queue_name, routing_key='#')
+
+     def callback(ch, method, properties, body):
+          updated_config = json.loads(body.decode('utf-8'))
+          print(updated_config)
+          if(updated_config.get('destinationService','xyz').startswith("article")):
+               updateConfigEnv()
+
+     channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+     print(' [*] Waiting for messages. To exit press CTRL+C')
+     channel.start_consuming()
 
 startListening()
